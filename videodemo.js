df9b87ef7f98bf0f5ffb3f2df5a80223f9bbb86a
jgraykeyin <justin.gray@keyin.com>
2021-02-10 14:56:59 -0330

    Some styling added to links

--- videodemo.js
+++ videodemo.js
@@ -1,12 +1,17 @@
 function main() {
 
-    console.log("Starting");
+    // Set focus on the first link so it's highlighted
+    document.getElementById("link-lsn1").focus();
+
+    
     let video = document.getElementById("vidframe");
+    let vid_title = document.getElementById("video_title");
 
     // Setting up click events for the Lesson links
     let lesson1 = document.getElementById("link-lsn1");
     lesson1.addEventListener("click", function(){
 
+        vid_title.innerHTML = "Lesson 1 Title Heading";
         // We'll need to insert a new video here
         video.src = "https://www.youtube.com/embed/5MgBikgcWnY"
 
@@ -15,6 +20,7 @@ function main() {
     let lesson2 = document.getElementById("link-lsn2");
     lesson2.addEventListener("click", function(){
 
+        vid_title.innerHTML = "Lesson 2 Title Heading";
         video.src = "https://www.youtube.com/embed/TQMbvJNRpLE"
 
     });
@@ -22,6 +28,7 @@ function main() {
     let lesson3 = document.getElementById("link-lsn3");
     lesson3.addEventListener("click", function(){
 
+        vid_title.innerHTML = "Lesson 3 Title Heading";
         video.src = "https://www.youtube.com/embed/w-HYZv6HzAs"
 
     });
@@ -29,12 +36,14 @@ function main() {
     let lesson4 = document.getElementById("link-lsn4");
     lesson4.addEventListener("click", function(){
 
+        vid_title.innerHTML = "Lesson 4 Title Heading";
         video.src = "https://www.youtube.com/embed/Nj-hdQMa3uA"
     });
 
     let lesson5 = document.getElementById("link-lsn5");
     lesson5.addEventListener("click", function(){
 
+        vid_title.innerHTML = "Lesson 5 Title Heading";
         video.src = "https://www.youtube.com/embed/TFbv757kup4"
     });
 }
