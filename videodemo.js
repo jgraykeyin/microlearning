621a195d64ede5e8a69ca10206037660f498e8c6
jgraykeyin <justin.gray@keyin.com>
2021-02-10 09:20:37 -0330

    Added a click-event listener to the first Lesson link

---
+++ videodemo.js
@@ -0,0 +1,16 @@
+function main() {
+
+    console.log("Starting");
+
+    // Setting up click events for the Lesson links
+    let lesson1 = document.getElementById("link-lsn1");
+    lesson1.addEventListener("click", function(){
+
+        // We'll need to insert a new video here
+        console.log("Hey hey hey hey");
+
+    });
+
+}
+
+window.addEventListener("load", main);
