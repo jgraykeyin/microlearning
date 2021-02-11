b49cf8375673588479290571ca12bff0cd0db1ae
Mark <71827938+Mark0195@users.noreply.github.com>
2021-02-11 11:43:17 -0330

    Add files via upload

---
+++ Quiz_demo_2.js
@@ -0,0 +1,105 @@
+
+  function buildQuiz(){
+    const output = [];
+
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        // variable to store the list of possible answers
+        const answers = [];
+        for(letter in currentQuestion.answers){
+
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+
+    // combine output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults(){
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "What is 2 + 2?",
+      answers: {
+        a: "4",
+        b: "22",
+        
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is 7 * 3?",
+      answers: {
+        a: "30",
+        b: "21",
+        c: "14",
+        d: "30"
+
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What is 4 * 2?",
+      answers: {
+        a: "6",
+        b: "8",
+        
+      },
+      correctAnswer: "b"
+    }
+  ];
+
+  
+  buildQuiz();
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
