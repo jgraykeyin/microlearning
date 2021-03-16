7f7486e83706b6aaf6f1d923147617ce88fd6025
jgraykeyin <justin.gray@keyin.com>
2021-03-16 15:14:05 -0230

    Setup a login form on the login page with some js

---
+++ login_testing.js
@@ -0,0 +1,10 @@
+function main() {
+    loginBtn = document.getElementById("login-btn");
+    loginBtn.addEventListener("click", function() {
+        alert("Logged in!!");
+    });
+}
+
+
+
+window.addEventListener("load", main);
