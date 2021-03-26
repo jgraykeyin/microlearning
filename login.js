2ef3d9834dd4a27c608a92a27cd7da5aee9e4333
lloveys <lyndon.loveys@keyin.com>
2021-03-26 13:16:31 -0230

    I needed to add this update before doing another pull

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ let path = require('path')
 let bcrypt = require('bcrypt');
 let https = require('https');
 
-const USE_HTTPS = true;
+const USE_HTTPS = false;
 const PORT = 3000;
 const SSL_KEY_FILE_LOCATION = path.join(__dirname, 'ssl/key.pem')
 const SSL_CERT_FILE_LOCATION = path.join(__dirname, 'ssl/cert.pem')
