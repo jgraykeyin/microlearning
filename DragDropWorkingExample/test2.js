f2185d2022fddd31e39867ea7f98a2d77aa20de1
lloveys <lyndon.loveys@keyin.com>
2021-04-21 09:40:45 -0230

    submittable drag drop

--- DragDropWorkingExample/test2.js
+++ DragDropWorkingExample/test2.js
@@ -2,17 +2,17 @@ let draggable = document.querySelectorAll(".draggable");
 const containers = document.querySelectorAll(".container")
 
 draggable.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
+    draggable.addEventListener('touchstart', () => {
         draggable.classList.add('dragging')
         console.log('drag start')
     })
-    draggable.addEventListener('dragend', () => {
+    draggable.addEventListener('touchend', () => {
         draggable.classList.remove('dragging')
     })
 })
 
 containers.forEach(container => {
-    container.addEventListener('dragover', (e) => {
+    container.addEventListener('touchmove', (e) => {
         e.preventDefault()
         const afterElement = getDragAfterElement(container, e.clientY)
         const draggable = document.querySelector('.dragging')
@@ -27,11 +27,13 @@ containers.forEach(container => {
 })
 
 
-function getDragAfterElement(container, y){
+function getDragAfterElement(container, y, ){
     const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+    
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect()
         const offset = y - box.top - box.height / 2
+        
         
         if (offset < 0 && offset > closest.offset){
             return { offset: offset, element: child }
@@ -41,7 +43,18 @@ function getDragAfterElement(container, y){
     },{offset: Number.NEGATIVE_INFINITY }).element
 }
 
-   
+function submit(){
+    if (document.getElementById("tpCont").querySelector("#tp1") &&
+        document.getElementById("tpCont").querySelector("#tp2") && 
+        document.getElementById("tpCont").querySelector("#tp3") &&
+        document.getElementById("epCont").querySelector('#ep4') &&
+        document.getElementById("epCont").querySelector('#ep5') &&
+        document.getElementById("epCont").querySelector('#ep6')) {
+        console.log('yup')
+    } else {
+        console.log('nope')
+    }
+}
 
 
 
