383cbe286aedf225597f596988b588bddfcfa5b3
lloveys <lyndon.loveys@keyin.com>
2021-04-27 09:18:56 -0230

    updated json files to reflect checkbox MC inclusion for identification, added salt to login.js and connected to mysql to run node, finally combined checkbox MC to perfomance.html

--- login.js
+++ login.js
@@ -26,11 +26,12 @@ connection.query(`CREATE TABLE IF NOT EXISTS \`accounts\` (
     \`id\` int(11) NOT NULL,
     \`username\` varchar(50) NOT NULL,
     \`password\` varchar(255) NOT NULL,
-    \`email\` varchar(100) NOT NULL
+    \`email\` varchar(100) NOT NULL,
+    \`salt\` varchar(255) NOT NULL
   ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;`);
 
-/*connection.query(`ALTER TABLE \`accounts\` ADD PRIMARY KEY (\`id\`)`);  */
-/* connection.query(`ALTER TABLE \`accounts\` MODIFY \`id\` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;`); */
+connection.query(`ALTER TABLE \`accounts\` ADD PRIMARY KEY (\`id\`)`);  
+connection.query(`ALTER TABLE \`accounts\` MODIFY \`id\` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;`); 
 
 
 let app = express();
